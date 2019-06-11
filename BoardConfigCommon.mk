c7a71687f20ce27dd95b3c46f32e4e443c4dcc14
shagbag913 <sh4gbag913@gmail.com>
2019-06-11 02:05:45 -0400

    marlin/sailfish: always use highest clang version
    Signed-off-by: shagbag913 <sh4gbag913@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -9,7 +9,7 @@ TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_CONFIG := pa_marlin_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/marlin
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := 6.0.2
+TARGET_KERNEL_CLANG_VERSION := $(shell grep -v based prebuilts/clang/host/$(HOST_OS)-x86/*/AndroidVersion.txt | sort | tail -n 1 | cut -d : -f 2)
 
 # GPS
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
